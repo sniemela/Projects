692dc2eb3fd0157025e9f779c04a266f60fdbeb8
Simo Niemelä <simo.niemela@gmail.com>
2016-07-07 18:59:13 +0300

    Find PI to the Nth digit

---
+++ numbers/find_pi_to_nth_digit.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+    "math"
+    "strings"
+	"strconv"
+)
+
+const decimalLimit = 1000000
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter a number: ")
+	text, err := reader.ReadString('\n')
+    if err != nil {
+        fmt.Print("Could not read input.")
+        return
+    }
+
+    number, err := strconv.Atoi(strings.TrimRight(text, "\r\n"))
+    if err != nil {
+        fmt.Print("Could not convert text to integer. Error: ", err)
+        return
+    }
+
+    pi := calcPi()
+
+    textToPrint := "Calculated PI = %." + strconv.Itoa(number) + "f"
+    fmt.Printf(textToPrint, pi)
+}
+
+// Find PI to the Nth digit. Based on John Machin's formula.
+func calcPi() float64 {
+    return 16 * math.Atan(1 / 5.0) - 4 * math.Atan(1 / 239.0)
+}
