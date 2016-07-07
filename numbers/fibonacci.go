2f000837d4a6fbf7e4ae1c4965b30f5e357837c5
Simo Niemelä <simo.niemela@gmail.com>
2016-07-07 19:51:29 +0300

    Format code2

--- numbers/fibonacci.go
+++ numbers/fibonacci.go
@@ -1,23 +1,24 @@
 package main
 
 import (
-    "fmt"
-    "github.com/sniemela/Projects/util"
+	"fmt"
+
+	"github.com/sniemela/Projects/util"
 )
 
 func main() {
-    number, err := util.ReadNumber("Enter a number: ")
-    if err != nil {
-        fmt.Print("Could not read a number.")
-        return
-    }
+	number, err := util.ReadNumber("Enter a number: ")
+	if err != nil {
+		fmt.Print("Could not read a number.")
+		return
+	}
 
-    fmt.Printf("Fibonacci %d = %d", number, fib(number))
+	fmt.Printf("Fibonacci %d = %d", number, fib(number))
 }
 
 func fib(n int) int {
-    if n < 2 {
-        return n
-    }
-    return fib(n - 1) + fib(n - 2)
+	if n < 2 {
+		return n
+	}
+	return fib(n-1) + fib(n-2)
 }
