b2af3e1e994d9c4f45c818ddf52cbbad0b89a019
Simo Niemelä <simo.niemela@gmail.com>
2016-07-07 19:00:54 +0300

    Format code

--- numbers/find_pi_to_nth_digit.go
+++ numbers/find_pi_to_nth_digit.go
@@ -3,10 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
-    "math"
-    "strings"
 	"strconv"
+	"strings"
 )
 
 const decimalLimit = 1000000
@@ -15,24 +15,24 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter a number: ")
 	text, err := reader.ReadString('\n')
-    if err != nil {
-        fmt.Print("Could not read input.")
-        return
-    }
+	if err != nil {
+		fmt.Print("Could not read input.")
+		return
+	}
 
-    number, err := strconv.Atoi(strings.TrimRight(text, "\r\n"))
-    if err != nil {
-        fmt.Print("Could not convert text to integer. Error: ", err)
-        return
-    }
+	number, err := strconv.Atoi(strings.TrimRight(text, "\r\n"))
+	if err != nil {
+		fmt.Print("Could not convert text to integer. Error: ", err)
+		return
+	}
 
-    pi := calcPi()
+	pi := calcPi()
 
-    textToPrint := "Calculated PI = %." + strconv.Itoa(number) + "f"
-    fmt.Printf(textToPrint, pi)
+	textToPrint := "Calculated PI = %." + strconv.Itoa(number) + "f"
+	fmt.Printf(textToPrint, pi)
 }
 
 // Find PI to the Nth digit. Based on John Machin's formula.
 func calcPi() float64 {
-    return 16 * math.Atan(1 / 5.0) - 4 * math.Atan(1 / 239.0)
+	return 16*math.Atan(1/5.0) - 4*math.Atan(1/239.0)
 }
